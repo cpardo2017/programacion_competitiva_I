0e76f66e644b3e1cda640a1c93f5632229361b91
carlos <carlospardosantos@gmail.com>
2018-04-20 14:47:46 -0300

    2018-1

--- 2018-1/enProceso/UVA482.c
+++ 2018-1/enProceso/UVA482.c
@@ -1,20 +1,26 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <assert.h>
 
 int main()
 {
 	int n,i,c,num,a,b,aux;
 	int orden[20];
-	float numeros[20];
+	double numeros[20];
 
 	scanf("%d",&n);
+	c=fgetc(stdin);
+	assert(c=='\n');
+
 
 	for(i=0;i<n;i++)
 	{
+		c=fgetc(stdin);
+		assert(c=='\n');
 		num=0;
 		a=0;
 
-		while((c=fgetc(stdin)!='\n'))
+		while((c=fgetc(stdin))!='\n')
 		{
 			if(c>='0' && c<='9')
 			{
@@ -29,16 +35,24 @@ int main()
 			}
 		}
 
-		for(b=0;b<=a;b++)
+		orden[a]=num;
+		num=0;
+		a++;
+
+		for(b=0;b<a;b++)
 		{
-			scanf("%f",&numeros[b]);
+			aux=orden[b]-1;
+			scanf("%lf",&numeros[aux]);
+		}
+		c=fgetc(stdin);
+		assert(c=='\n');
+
+		for(b=0;b<a;b++)
+		{
+			printf("%lf\n",numeros[b]);
 		}
 
-		for(c=0;c<=a;c++)
-		{
-			printf("%f\n",numeros[orden[c]] );
-		}
-
+		printf("\n");
 
 	}
 
